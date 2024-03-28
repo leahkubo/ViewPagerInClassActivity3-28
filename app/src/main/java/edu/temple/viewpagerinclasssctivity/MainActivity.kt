28f6dbffbcca3c0db74e5515114c654e170be7bd
leahkubo <kubalaleah@gmail.com>
2024-03-28 11:37:35 -0400

    Implemented ViewPager2 usage

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -12,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+
+        viewPager.adapter = object: FragmentStateAdapter(this){
+            override fun getItemCount() = 10
+
+            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
+
+        }
+
     }
+
 
 }
